bc862c4c6142db4d8c24abf163d38a1d282ddcaf
HiepTV <35910113+ffse1701003@users.noreply.github.com>
2019-09-11 10:00:08 +0700

    nothing

--- application/config/custom_configuration.php
+++
@@ -1,6 +0,0 @@
-<?php
-$config['base_url_admin'] 	= 'http://localhost/pando/admin/';
-$config['PLUGIN_PATH']		= 'http://localhost/pando/plugins/';
-$config['IMG_PATH']			= 'http://localhost/pando/assets/images/';
-$config['THUMB_DEFAULT']	= $config['IMG_PATH'].'no_image.jpg';
-?>
